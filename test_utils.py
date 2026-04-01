fad4aad9d24d097d2569ba9aa25ee2076fff2edb
jawaun <jawaun.brown95@gmail.com>
2026-04-01 15:32:01 -0400

    feat: add hello utility

--- test_utils.py
+++ test_utils.py
@@ -21,3 +21,4 @@ def filter_outliers(data: list, threshold: float = 2.0) -> list:
     mean = statistics.mean(data)
     stdev = statistics.stdev(data)
     return [x for x in data if abs(x - mean) <= threshold * stdev]
+def hello(): return 'world' # Wed Apr  1 15:32:01 EDT 2026
